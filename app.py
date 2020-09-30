41a97bcadc3d1cd616f2264e3e393d6ba2fb9cdf
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2020-09-29 21:06:54 -0300

    inicio do processo de layout da página

--- app.py
+++ app.py
@@ -6,41 +6,65 @@ import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
 
+app = dash.Dash(__name__, title='Fga Health Analytics', update_title='Carregando...')
 #Defindo Dataframe
 df_global = pd.read_excel('covid_global.xlsx')
 
-
 df_global = df_global[df_global['location']=='Afghanistan']
 
-#Definindo Gráficos
+#Definindo Gráfico 1 
 fig_bar_global = go.Figure( data = [
     go.Bar(x = df_global['date'], y = df_global['total_cases'], name ='Casos', marker_color = "yellow"),
     go.Bar(x = df_global['date'], y = df_global['total_deaths'], name ='Mortes', marker_color = "red"),
 ])
-fig_bar_global.update_layout(barmode='overlay')
-fig_bar_global.show()
+fig_bar_global.update_layout(
+    barmode='overlay',
+    height=275,
+    margin=dict(
+        l=25,
+        r=25,
+        b=25,
+        t=25,
+    ),
+)
 
-#fig_bar_global.show()
+#Definindo Gráfico 2
+fig_bar_global_2 = go.Figure( data = [
+    go.Bar(x = df_global['date'], y = df_global['total_cases'], name ='Casos', marker_color = "yellow"),
+    go.Bar(x = df_global['date'], y = df_global['total_deaths'], name ='Mortes', marker_color = "red"),
+])
+fig_bar_global_2.update_layout(
+    barmode='overlay',
+    height=275,
+    margin=dict(
+        l=25,
+        r=25,
+        b=25,
+        t=25,
+    ),
+)
 
-#data = [barra_global]
+app.layout = html.Div(children=[
+    html.Div(
+        id='header',
+        children=[
+            html.Img(src='/assets/logo_fga.png'),
+            html.P('Hello Dash'),
+        ]
+    ),
 
-#layout = dict(title='Global Covid', showlegend=False)
-#
-#fig = dict(data=data, layout=layout)
+    dcc.Graph(
+        id='example-graph',
+        figure=fig_bar_global
+    ),
+
+    dcc.Graph(
+        id='example-graph_2',
+        figure=fig_bar_global_2
+    )
+])
 
 
 
-#
-#app = dash.Dash()
-#
-#app.layout = html.Div([
-#    html.Div(html.H1(children="Hello World")),
-#    html.Label("Dash Graph"),
-#    html.Div(
-#        dcc.Graph(id="Global Covid",
-#        figure=fig)
-#    )
-#])
-#
-#if __name__=="__main__":
-#    app.run_server(debug=True) 
+if __name__=="__main__":
+    app.run_server(debug=True) 
