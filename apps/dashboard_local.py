f4ae59bb5c788452f408639d129d58c579f5906d
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2020-11-17 21:11:48 -0300

    início dashboard local

--- apps/dashboard_local.py
+++ apps/dashboard_local.py
@@ -11,16 +11,342 @@ from dash.exceptions import PreventUpdate
 import datetime
 from datetime import date
 import pathlib
+import numpy as np
 
 from app import app
 
 #Definindo dataframe
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../datasets").resolve()
-df_global = pd.read_excel(DATA_PATH.joinpath("covid_global_trans.xlsx"))
+df_local = pd.read_excel(DATA_PATH.joinpath("covid_brasil.xlsx"))
 
 layout = html.Div(children=[
-    html.Img(
-        src='../assets/logo_fga.png',
-    ),    
+    html.Div(
+        id='pop_up_global_message', #Div utilizada para avisar usuários de seleções que não são permitidas
+        children=[
+            html.H1('Aviso!',
+                id='title_global_pop_up',
+            ),
+            html.P(' ',
+                id='text_global_pop_up',
+            ),
+        ],
+        hidden=True, #Até ser alterada para False essa div não é plotada
+    ),
+    html.Div(
+        id='block_1',
+        children=[
+            html.Div( #Div inserida dentro da secção de filtros, para lidar com o texto "O que deseja ver?"
+                id='seccao_filtros',
+                children=[
+                    html.Div(
+                        id = "oq_deseja_ver", #Texto o que deseja ver?
+                        children=[
+                            html.P('O que deseja ver ?'), #Como o usuário verá
+                        ]
+                    ),
+                    html.Div( # Div inserida para colocar o texto "Gráfico 1"
+                        id="grafico_1_filtro_text",
+                        children=[
+                            html.P('Gráfico 1'), #Como o usuário verá
+                        ]
+                    ),
+                    html.Div( # Div inserida para colocar o texto "Gráfico 2"
+                        id="grafico_2_filtro_text",
+                        children=[
+                            html.P('Gráfico 2'), #Como o usuário verá
+                        ]
+                    ),
+                    
+                    html.Div(
+                        id='Primeira_linha',
+                        children=[
+                            dcc.Dropdown(id = 'pais_grafico_1',
+                                options = [{'label': i, 'value': i} for i in np.sort(df_local['estado'].dropna().unique())], 
+
+                                optionHeight = 35,            #Espaço entre as opções do dropdown
+                                value  = 'RO',                #Opção padrão ao iniciar a página
+                                disabled = False,             #Capacidade de interagir com o dropdown
+                                multi = False,                #Permitir múltiplas escolhas 
+                                searchable = True,            #Permitir digitar para procurar valor
+                                placeholder = 'Selecione...', #Frase que aparece quando nada foi selecionado
+                                clearable = True,             #Permitir que seja apagado o valor escolhido
+                                    #classname = '',               #Extrai a calsse de algum documento css dentro da pasata assets
+                                persistence = True,           #Mantem o valor até que , no type memory, a página dê um refresh
+                                persistence_type = 'memory',
+                                style={
+                                    'margin-top':'22px',
+                                },
+                            ),
+
+                            dcc.Dropdown(id = 'pais_grafico_2', #Antes grafico2_dado1
+                                options = [{'label': i, 'value': i} for i in np.sort(df_local['estado'].dropna().unique())], 
+                                #options: Leitura da coluna location da planilha, para evitar repetição o unique
+                                optionHeight = 35,
+                                value  = 'PR',
+                                disabled = False,
+                                multi = False,                
+                                searchable = True,
+                                placeholder = 'Selecione...',
+                                clearable = True,
+                                persistence = True,
+                                persistence_type = 'memory',
+                                style={
+                                    'margin-top':'22px',
+                                },
+                            ),      
+                        ],
+                    ),
+
+                    html.Div(
+                        id='Segunda_linha',
+                        children=[
+                            dcc.Dropdown(id = 'casos_mortes_grafico_1',
+                                options = [
+                                    {'label': 'Casos', 'value':'grafico_casos' },
+                                    {'label': 'Mortes', 'value': 'grafico_mortes'}], 
+
+                                optionHeight = 35,
+                                value  = ['grafico_casos', 'grafico_mortes'],
+                                disabled = False,
+                                multi = True,
+                                searchable = False,
+                                placeholder = 'Selecione...',
+                                clearable = False,
+                                persistence = True,
+                                persistence_type = 'memory',
+                                style={
+                                    'margin-top':'10px',
+                                    'font-size':'15px',
+                                },
+                            ),
+
+                            dcc.Dropdown(id = 'casos_mortes_grafico_2', 
+                                options = [
+                                    {'label': 'Casos', 'value':'grafico_casos' },
+                                    {'label': 'Mortes', 'value': 'grafico_mortes'}], 
+
+                                optionHeight = 35,
+                                value  = ['grafico_casos', 'grafico_mortes'],
+                                disabled = False,
+                                multi = True,                
+                                searchable = False,
+                                placeholder = 'Selecione...',
+                                clearable = False,
+                                persistence = True,
+                                persistence_type = 'memory',
+                                style={
+                                    'margin-top':'10px',
+                                    'font-size':'15px',
+                                },
+                            ),
+                        ],
+                    ),
+                    
+                    html.Div(
+                        id='terceira_linha',
+                        children=[
+                            dcc.Dropdown(id = 'tipo_grafico_1',
+                                options = [
+                                    {'label': 'Barra', 'value':'grafico_barra' },
+                                    {'label': 'Linha', 'value': 'grafico_linha'},
+                                    {'label': 'Mapa', 'value': 'grafico_mapa'}
+                                ], 
+
+                                optionHeight = 35,
+                                value  = 'grafico_barra',
+                                disabled = False, 
+                                multi = False,
+                                searchable = False,
+                                placeholder = 'Selecione...',
+                                clearable = False,
+                                persistence = True,
+                                persistence_type = 'memory',
+                                style={
+                                'margin-top':'10px',
+                                },
+                            ),
+
+                            dcc.Dropdown(id = 'tipo_grafico_2',
+                                options = [
+                                    {'label': 'Barra', 'value':'grafico_barra' },
+                                    {'label': 'Linha', 'value': 'grafico_linha'},
+                                    {'label': 'Mapa', 'value': 'grafico_mapa'}
+                                ], 
+
+                                optionHeight = 35,
+                                value  = 'grafico_barra',
+                                disabled = False, 
+                                multi = False,
+                                searchable = False,
+                                placeholder = 'Selecione...',
+                                clearable = False,
+                                persistence = True,
+                                persistence_type = 'memory',
+                                style={
+                                'margin-top':'10px',
+                                },
+                            ),
+                        ],
+                    ),
+                    
+                    html.Div(
+                        id='filtros_gerais_text',
+                        children=[
+                            html.P('Filtros Gerais'),
+                        ],
+                    ),
+
+                    html.Div(
+                        id='Quarta_linha', 
+                        style={
+                            'fontSize':'1px',
+                        },
+                        children=[
+                            dcc.DatePickerRange(
+                                id='escolha_data',
+                                min_date_allowed=date(2020, 1, 1),
+                                max_date_allowed=date(2020, 12, 24),
+                                #initial_visible_month=date(2020, 3, 10),
+                                start_date=date(2020, 2, 14),
+                                end_date=date(2020, 6, 20),
+                            ),
+    
+                            html.Div(id='output-container-date-picker-range'),
+                        ],
+                    ),
+                    html.Div(
+                        children=[
+                            html.Button(
+                                'GO!',
+                                id='Submit_button',
+                                n_clicks=0,
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+
+            html.Div(
+                id='top_3',
+                children=[
+                    dcc.Graph(
+                        id='top3_global',
+                        config={
+                            'displayModeBar': False,
+                            'displaylogo': False,
+                            'modeBarButtonsToRemove': [
+                                'zoom2d', 'pan2d', 'lasso2d', 'select2d', 'zoomIn2d', 'zoomOut2d',
+                                'toggleSpikelines',
+                            ],
+                        },
+                        style = {'border-radius': 30,},
+                    ),
+                ],
+            ),
+        ]
+    ),
+
+    html.Div( #Bloco de divs da direita --> Resumo geral, grafico 1 e grafico 2.
+        className='resumo_geral',
+        children=[
+            html.Div(
+                id='resumo_casos',
+                children=[
+                    html.Div(
+                        id='icon_circle_casos',
+                    ),
+
+                    html.Img(
+                        id='icon_casos',
+                        src='/assets/icon_casos.svg',
+                    ),
+
+                    html.P(
+                        'Casos Confirmados',
+                        id='casos_confirmados_text',
+                    ),
+
+                    html.P(
+                        'Acumulado',
+                        id='acumulado_casos_text',
+                    ),
+
+                    html.P(
+                        'Novos Casos',
+                        id='novos_casos_text',
+                    ),
+                ],
+            ),
+            html.Div(
+                id='colocar_algo',
+            ),
+            html.Div(
+                id='resumo_obitos',
+                children=[
+                    html.Div(
+                        id='icon_circle_obitos',
+                    ),
+
+                    html.Img(
+                        id='icon_obitos',
+                        src='/assets/icon_deaths.svg',
+                    ),
+
+                    html.P(
+                        'Óbitos Confirmados',
+                        id='obitos_confirmados',
+                    ),
+
+                    html.P(
+                        'Acumulado',
+                        id='acumulado_obitos_text',
+                    ),
+
+                    html.P(
+                        'Letalidade',
+                        id='letalidade_text',
+                    ),
+
+                    html.P(
+                        'Novos Óbitos',
+                        id='novos_obitos_text'
+                    ),
+                ],
+            ),
+
+            html.Div(
+                id='grafico_1',
+                children=[
+                    dcc.Graph(
+                        id='grafico-1',
+                        config={
+                            'displaylogo': False,
+                            'modeBarButtonsToRemove':[
+                                'lasso2d', 'select2d', 'zoomIn2d', 'zoomOut2d',
+                                'toggleSpikelines',
+                            ],
+                        }, 
+                    ),
+                ],
+            ),
+
+            html.Div(
+                id='grafico_2',
+                children=[
+                    dcc.Graph(
+                        id='grafico-2',
+                        config={
+                            'displaylogo': False,
+                            'modeBarButtonsToRemove':[
+                                'lasso2d', 'select2d', 'zoomIn2d', 'zoomOut2d',
+                                'toggleSpikelines',
+                            ],
+                        }, 
+                    ),
+                ],
+            ),
+        ],
+    ),
+
 ])
