f5ea3cfa9e0f2b64bd23db5635e51d2efad77888
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2020-11-22 17:32:54 -0300

    Alteração do df_top3 para formato de listas, e implementação na dashboard global

--- test_lista.py
+++ test_lista.py
@@ -8,11 +8,14 @@ df_global = pd.read_excel(DATA_PATH.joinpath("covid_global_trans.xlsx"))
 new_end_date_df1 = df_global[df_global.date == '22/08/2020']
 
 
-df_global_top3 = new_end_date_df1.values.tolist()
+df = new_end_date_df1.values.tolist()
 
+for i in range(len(df)):
+    for h in range(0, len(df)-i-1 ):
+        if df[h][6] < df[h+1][6]:
+            df[h], df[h+1] = df[h+1], df[h]
+for i in range(len(df)):
+    if df[i][2] == "Mundo":
+        df[i] = None 
 
-count = 0
-aux = []
-for i in range(len(df_global_top3)):
-    df_global_top3[i] = [df_global_top3[i][2], df_global_top3[i][6]]
-    #(df_global_top3[i][2], df_global_top3[i][6])
+print(df[0:4])
